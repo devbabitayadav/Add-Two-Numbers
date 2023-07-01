6fad8e299989835f56f95cbc29774e13c24e38cc
“devbabitayadav” <babitayadav9584@gmail.com>
2023-07-01 13:49:14 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,13 @@
+// Write a JavaScript program to add two numbers and display the result.
+
+function add()
+{
+  var numOne, numTwo, sum;
+  numOne = parseInt(document.getElementById("first").value);
+  numTwo = parseInt(document.getElementById("second").value);
+  sum = numOne + numTwo;
+  document.getElementById("answer").value = sum;
+}
+
+
+
